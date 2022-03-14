c12ce1b1da1161b3b01a283a6e83f6b161dd6ade
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-03-14 21:20:14 +0200

    удаление ненужного

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Message from "./components/Message";
 import './App.css';
 
-function App() {
+function App(props) {
+  const message = 'HI ALL';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Message msg={message} />
+    </div >
   );
 }
 
 export default App;
+
---
+++ src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Message = function (props) {
+    return (
+        <div>
+            {props.msg}
+        </div>
+    );
+}
+
+export default Message;
