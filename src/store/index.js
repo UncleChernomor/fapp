3c70d8f8311c79426075961668c1f90785e4a008
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-04-18 16:43:01 +0300

    add redux-presist and state add to presist

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import thunk from 'redux-thunk';
 import chatsReducer from "./chats/chatsReducer";
 import messagesReducer from "./messages/messagesReducer";
 import profileReducer from "./profile/reducerProfile";
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
     profile: profileReducer,
@@ -11,5 +12,14 @@ const rootReducer = combineReducers({
     messages: messagesReducer,
 });
 
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+export const store = createStore(pReducer, composeEnhancers(applyMiddleware(thunk)));
+export const persistor = persistStore(store);
