4662f983db6bc273266cd225d471150b5ef0bb3b
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-04-18 17:03:55 +0300

    create page WorkWithApi. Added Links for page

---
+++ src/pages/WorkWithApi.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ButtonV from '../components/UI/button/ButtonV';
+
+function WorkWithApi(props) {
+    return (
+        <div>
+            <ButtonV>
+                Get DATA
+            </ButtonV>
+            <h1>work with API</h1>
+        </div>
+    );
+}
+
+export default WorkWithApi;
