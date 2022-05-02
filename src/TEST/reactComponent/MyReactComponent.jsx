e2ec55d47449f8aa891c7b7f297263e798ae8785
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-05-02 13:02:40 +0300

    delete debug jest

---
+++ src/TEST/reactComponent/MyReactComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyReactComponent(props) {
+    return (
+        <div>
+            <h1>hi all</h1>
+            <input placeholder='input text' />
+            <button>click me</button>
+        </div>
+    );
+}
+
+export default MyReactComponent;
--- src/TEST/reactComponent/MyReactComponent.test.js
+++ src/TEST/reactComponent/MyReactComponent.test.js
@@ -2,17 +2,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import MyReactComponent from './MyReactComponent';
 
-describe('TEST React Component: ', q() => {
+describe('TEST React Component: ', () => {
     test('elements are on the page', () => {
         render(<MyReactComponent />);
-const myText = screen.getByText(/hi all/i);
-const myInput = screen.getByPlaceholderText(/input text/i);
-const myButton = screen.getByRole('button');
+        const myText = screen.getByText(/hi all/i);
+        const myInput = screen.getByPlaceholderText(/input text/i);
+        const myButton = screen.getByRole('button');
 
-expect(myText).toBeInTheDocument();
-expect(myInput).toBeInTheDocument();
-expect(myButton).toBeInTheDocument();
-
-screen.debug();
+        expect(myText).toBeInTheDocument();
+        expect(myInput).toBeInTheDocument();
+        expect(myButton).toBeInTheDocument();
     });
 });
