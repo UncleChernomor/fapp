63d90a5d425e352a9d2095bd94c1c2c7b572ce18
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-05-02 12:59:57 +0300

    add test from simple react component

---
+++ src/TEST/reactComponent/MyReactComponent.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MyReactComponent from './MyReactComponent';
+
+describe('TEST React Component: ', q() => {
+    test('elements are on the page', () => {
+        render(<MyReactComponent />);
+const myText = screen.getByText(/hi all/i);
+const myInput = screen.getByPlaceholderText(/input text/i);
+const myButton = screen.getByRole('button');
+
+expect(myText).toBeInTheDocument();
+expect(myInput).toBeInTheDocument();
+expect(myButton).toBeInTheDocument();
+
+screen.debug();
+    });
+});
